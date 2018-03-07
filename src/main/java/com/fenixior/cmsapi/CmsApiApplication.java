f4797e7cce8dc27fe5776ea2ab5a8428b586c3a4
Tomasz Truszkowski <harpoon2009@gmail.com>
2018-03-07 22:24:46 +0100

    [0.1.2][API]Temporarly disabled Spring Security

--- src/main/java/com/fenixior/cmsapi/CmsApiApplication.java
+++ src/main/java/com/fenixior/cmsapi/CmsApiApplication.java
@@ -1,12 +1,15 @@
 package com.fenixior.cmsapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 public class CmsApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CmsApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CmsApiApplication.class, args);
+    }
 }
